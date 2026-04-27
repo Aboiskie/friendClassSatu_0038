4089c982f9c4011c929b605f833d20fed3a6b8cb
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-27 12:17:33 +0700

    menambahkan class Belahketupat

--- tugas.cpp
+++ tugas.cpp
@@ -1,28 +1,6 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
+// Forward declaration
 class BelahKetupat;
-
-class LayangLayang; {
-private:
-    int d1, d2;
-    int s;
-//lengkapi disini
-
-public:
-
-    //lengkapi disini
-    double hitungkeliling() {
-        return s * 4;
-    }
-
-    double hitungluas() {
-        return 0.5 * d1 * d2;
-    }
-
-    //Lengkapi disini
-
-};
-        
