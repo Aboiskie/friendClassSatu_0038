13906306038316f3b3e98d65f3fdca25f1768de2
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-27 12:19:58 +0700

    menambahkan metode input untuk sisi dan diagonal pada class LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -15,4 +15,12 @@ public:
         cout << "=== Layang-Layang ===" << endl;
         cout << "Masukkan diagonal 1: ";
         cin >> d1;
-        
+        cout << "Masukkan diagonal 2: ";
+        cin >> d2;
+        cout << "Masukkan sisi 1: ";
+        cin >> s1;
+        cout << "Masukkan sisi 2: ";
+        cin >> s2;
+    }
+
+    
