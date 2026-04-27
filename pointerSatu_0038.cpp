169d319c6b677cbb5322c240657373d4dadd7778
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-27 12:09:02 +0700

    menambahkan implementasi fungsi main untuk pengujian class 'mahasiswa'

--- pointerSatu_0038.cpp
+++ pointerSatu_0038.cpp
@@ -9,3 +9,12 @@ public:
     }
 };
 
+int main() {
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    refMhs.showNim();
+
+    
