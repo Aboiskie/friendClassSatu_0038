aabbacdf13a0d8e94171c33afe77f732d2a5b639
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-27 12:06:43 +0700

    menambah class pelajar

--- friendClassDua_0038.cpp
+++ friendClassDua_0038.cpp
@@ -3,32 +3,3 @@ using namespace std;
 
 class pelajar; // Forward declaration
 
-class manusia
-{
-public:
-    void showNilaiPelajar(pelajar &x);
-};
-
-class pelajar
-{
-private:
-    int nilai;
-
-public:
-    pelajar() { nilai = 100; }
-    friend void manusia::showNilaiPelajar(pelajar &x); // Friend function
-};
-
-void manusia::showNilaiPelajar(pelajar &x)
-{
-    cout << x.nilai;
-}
-
-int main()
-{
-    manusia budi;
-    pelajar pbudi;
-    budi.showNilaiPelajar(pbudi);
-
-    return 0;
-}
